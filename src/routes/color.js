0dbef7ae6aafdbf9f49a16cc15d7e52a99b1b6dc
uhmtoto <uhmtotow@gmail.com>
2019-08-06 19:39:35 +0900

    feat: add color route

--- src/routes/color.js
+++ src/routes/color.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+import { check, validationResult } from 'express-validator'
+
 import models from '@/models'
 
 const router = Router()
@@ -9,6 +11,24 @@ router.get('/', (req, res, next) => {
     .then(r => res.json({
       colors: r
     }))
+})
+
+router.post('/', [
+  check('name').isString().not().isEmpty(),
+  check('introduce').isString().not().isEmpty(),
+  check('rgb').isString().not().isEmpty().isLength({ min: 6, max: 6 }),
+  check('kelvin').isNumeric().not().isEmpty()
+], (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return next(errors)
+
+  let newColor = new models.Color()
+  newColor = Object.assign(newColor, req.body)
+
+  newColor.save()
+    .then(r => res.json({
+      color: r
+    })).catch(e => next(e))
 })
 
 export default router
