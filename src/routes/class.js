3867ce2cc4383196fa90e2bf8255c47deff0ecf7
uhmtoto <uhmtotow@gmail.com>
2019-08-06 19:27:01 +0900

    feat: class delete route and functionify permission check logic

--- src/routes/class.js
+++ src/routes/class.js
@@ -3,15 +3,25 @@ import { Router } from 'express'
 import { check, validationResult } from 'express-validator'
 
 import models from '@/models'
+import responses from '@/utils/responses'
 
 const router = Router()
+
+function checkPermission (klass, user) {
+  if (user.type === 'a') return true
+  else if (user.type === 's') {
+    return klass.students.includes(user._id)
+  } else { // teacher
+    return user.class.includes(klass._id)
+  }
+}
 
 router.get('/', (req, res, next) => {
   models.Class.find({})
     .then(r => {
-      r = r.filter(v => {
-        return v.students.includes(req.user._id) ||
-          req.user.class.includes(v._id)
+      r = r.filter(v => checkPermission(v, req.user))
+      res.json({
+        classes: r
       })
     }).catch(e => next(e))
 })
@@ -43,8 +53,19 @@ router.put('/:classId', (req, res, next) => {
 
 })
 
-router.delete('/:classId', (req, res, next) => {
+router.delete('/:classId', async (req, res, next) => {
+  const classId = req.params.classId
 
+  const klass = await models.Class.findById(classId)
+
+  if (!klass) return next(responses.classNotExist)
+  if (!checkPermission(klass, req.user)) {
+    return next(responses.noPermission)
+  }
+
+  klass.delete()
+    .then(() => res.status(204).end())
+    .catch(e => next(e))
 })
 
 export default router
