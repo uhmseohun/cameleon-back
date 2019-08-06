7cd6a6d54a6049a0fef0f790a5b6fb6abc6fc519
uhmtoto <uhmtotow@gmail.com>
2019-08-06 17:41:15 +0900

    feat: add auth response models

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -2,5 +2,13 @@ export default {
   checkAccount: {
     status: 404,
     message: '일치하는 계정이 없습니다.'
+  },
+  needAuth: {
+    status: 401,
+    message: '인증이 필요합니다.'
+  },
+  failAuth: {
+    status: 401,
+    message: '인증을 실패했습니다.'
   }
 }
