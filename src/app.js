569943db5fbb67261265952318b169ab5b9c7fe5
uhmtoto <uhmtotow@gmail.com>
2019-08-06 16:19:00 +0900

    fix: change route name

--- src/app.js
+++ src/app.js
@@ -18,11 +18,11 @@ mongoose.connect('mongodb://localhost:27017/cameleon',
 
 app.use(utils.middlewares.auth)
 
-app.use('/auth', routes.auth)
-app.use('/class', routes.class)
-app.use('/color', routes.color)
-app.use('/subject', routes.subject)
-app.use('/user', routes.user)
+app.use('/auth', routes.Auth)
+app.use('/class', routes.Class)
+app.use('/color', routes.Color)
+app.use('/subject', routes.Subject)
+app.use('/user', routes.User)
 
 app.use(bodyParser)
 app.use(cors)
