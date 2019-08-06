e83692b38d7c5e6cf4924261b9ba0aa910e60846
uhmtoto <uhmtotow@gmail.com>
2019-08-07 03:26:18 +0900

    feat: tag create route

--- src/routes/tag.js
+++ src/routes/tag.js
@@ -10,4 +10,24 @@ router.get('/', async (req, res, next) => {
   })
 })
 
+router.post('/', [
+  check('name').isString().not().isEmpty(),
+  check('payload').isNumeric().not().isEmpty(),
+  check('color').isString().not().isEmpty()
+], (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return next(errors)
+
+  let newTag = new models.Tag()
+  newTag = Object.assign(newTag, req.body)
+
+  newTag.save()
+    .then(r => {
+      res.json({
+        tag: r
+      })
+    })
+    .catch(e => next(e))
+})
+
 export default router
