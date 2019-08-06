9e377ff05021f4b1ceea7d961f4d2294789b9496
uhmtoto <uhmtotow@gmail.com>
2019-08-06 19:33:55 +0900

    feat: get color list route

--- src/routes/color.js
+++ src/routes/color.js
@@ -1,5 +1,14 @@
 import { Router } from 'express'
 
+import models from '@/models'
+
 const router = Router()
+
+router.get('/', (req, res, next) => {
+  models.Color.find({})
+    .then(r => res.json({
+      colors: r
+    }))
+})
 
 export default router
