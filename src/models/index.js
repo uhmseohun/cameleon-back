c8161fa78729762f549c1067280452f2baae1f94
uhmtoto <uhmtotow@gmail.com>
2019-08-06 17:11:22 +0900

    feat: error model

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,12 @@ import Class from './class'
 import Color from './color'
 import Subject from './subject'
 import User from './user'
+import Error from './error'
 
 export default {
   Class,
   Color,
   Subject,
-  User
+  User,
+  Error
 }
