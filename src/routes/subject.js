0396b5ade27ffe50ed47b951e440d1ba44099b2c
uhmtoto <uhmtotow@gmail.com>
2019-08-07 01:23:02 +0900

    feat: load all subjects route

--- src/routes/subject.js
+++ src/routes/subject.js
@@ -1,5 +1,13 @@
 import { Router } from 'express'
+import { check, validationResult } from 'express-validator'
+import models from '@/models'
 
 const router = Router()
+
+router.get('/', async (req, res, next) => {
+  res.json({
+    subjects: await models.Subject.find({})
+  })
+})
 
 export default router
