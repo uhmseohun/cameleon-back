192bcbfa7cc54663c3631fc70c0549ced43af36c
uhmtoto <uhmtotow@gmail.com>
2019-08-08 16:08:54 +0900

    style: eslint by standardjs

--- src/routes/arduino.js
+++ src/routes/arduino.js
@@ -1,20 +1,19 @@
 import { Router } from 'express'
 
-import responses from '@/utils/responses'
 import axios from 'axios'
 
 const router = Router()
 
 router.get('/', async (req, res, next) => {
-  function command(command){
+  function command (command) {
     axios.get(`https://us-central1-luna-ai-secretary.cloudfunctions.net/addInQueue/${command}`)
   }
 
-  const no = req.body.tag
+  const no = parseInt(req.body.tag)
 
-  if (no == 1) command('r')
-  if (no == 2) command('g')
-  if (no == 3) command('b')
+  if (no === 1) command('r')
+  if (no === 2) command('g')
+  if (no === 3) command('b')
 
   res.status(204).end()
 })
